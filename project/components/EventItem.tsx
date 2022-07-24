c2c8d1de18f369ab27406842d10c45c1a1b0dde0
sangminlee98 <tkdals0975@naver.com>
2022-07-24 15:49:01 +0900

    [project] react components props connecting

--- project/components/EventItem.tsx
+++ project/components/EventItem.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { IDummyEvent } from "../types";
 
@@ -6,10 +8,37 @@ interface IEventItem {
 }
 
 const EventItem = ({ item }: IEventItem) => {
+  const humanReadableDate = new Date(item.date).toLocaleDateString("ko", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const formattedAddress = item.location.replace(", ", "\n");
   return (
-    <div>
-      <p>{JSON.stringify(item)}</p>
-    </div>
+    <li>
+      <Image
+        src={"/" + item.image}
+        width="400px"
+        height="500px"
+        alt={item.id}
+      />
+      <div>
+        <div>
+          <h2>{item.title}</h2>
+          <div>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div>
+            <address style={{ wordBreak: "break-all" }}>
+              {formattedAddress}
+            </address>
+          </div>
+        </div>
+        <div>
+          <Link href={`/events/${item.id}`}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
   );
 };
 
