423fe218012186116904b1ddb86bc30a854679c6
sangminlee98 <tkdals0975@naver.com>
2022-08-22 00:13:15 +0900

    [ssr-practice] client-side data fetching과 ssg 결합하여 사용하기

--- ssr-practice/pages/last-sales.tsx
+++ ssr-practice/pages/last-sales.tsx
@@ -1,33 +1,40 @@
 import axios from "axios";
 import useSWR from "swr";
 import React, { useEffect, useState } from "react";
+import { GetStaticProps, NextPage } from "next";
 
 interface ISale {
   id: string;
   username: string;
   volume: string;
 }
-
-const LastSalesPage = () => {
-  // const [sales, setSales] = useState<ISale[] | undefined>();
+interface ILastSalesPageProps {
+  salesData: ISale[];
+}
+const LastSalesPage: NextPage<ILastSalesPageProps> = ({ salesData }) => {
+  const [sales, setSales] = useState<ISale[] | undefined>(salesData);
   // const [isLoading, setIsLoading] = useState(false);
 
-  const getSales = async () => {
+  const { data, error } = useSWR("salesList", async () => {
     const response = await axios.get(
       "https://nextjs-course-29d4b-default-rtdb.firebaseio.com/sales.json"
     );
-    const formatSales: ISale[] = [];
-    for (const key in response.data) {
-      formatSales.push({
-        id: key,
-        username: response.data[key].username,
-        volume: response.data[key].volume,
-      });
-    }
-    return formatSales;
-  };
-  const { data, error } = useSWR("sales", getSales);
+    return response.data;
+  });
 
+  useEffect(() => {
+    if (data) {
+      const formatSales: ISale[] = [];
+      for (const key in data) {
+        formatSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(formatSales);
+    }
+  }, [data]);
   // useEffect(() => {
   //   setIsLoading(true);
   //   axios
@@ -46,10 +53,10 @@ const LastSalesPage = () => {
   //     });
   // }, []);
   if (error) return <p>Failed to load.</p>;
-  if (!data) return <p>Loading...</p>;
+  if (!sales) return <p>Loading...</p>;
   return (
     <ul>
-      {data?.map((item) => (
+      {sales?.map((item) => (
         <li key={item.id}>
           {item.username} - ${item.volume}
         </li>
@@ -59,3 +66,22 @@ const LastSalesPage = () => {
 };
 
 export default LastSalesPage;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const response = await axios.get(
+    "https://nextjs-course-29d4b-default-rtdb.firebaseio.com/sales.json"
+  );
+  const formatSales: ISale[] = [];
+  for (const key in response.data) {
+    formatSales.push({
+      id: key,
+      username: response.data[key].username,
+      volume: response.data[key].volume,
+    });
+  }
+  return {
+    props: {
+      salesData: formatSales,
+    },
+  };
+};
